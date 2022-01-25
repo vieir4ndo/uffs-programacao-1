38d971d1d2d75c66a95a4eb1965fd20b6e196ab2
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-01-25 15:57:44 -0300

    Update Exe1.java

--- L2.3/Exe1.java
+++ L2.3/Exe1.java
@@ -1,8 +1,35 @@
 /*
+1) Você foi encarregado de fazer o diagrama UML de uma aplicação que representa o
+interpretador de uma linguagem de programação (você não precisa escrever código Java algum,
+apenas o diagrama UML). 
+
+Um interpretador sempre possui um vetor/array de variáveis. 
+
+Cada uma dessas variáveis possui um nome (string) e um valor (double). Uma variável não pode ter
+um nome vazio ou um valor menor que zero.
+
+Um interpretador também possui um vetor de strings que representa o conteúdo do arquivo sendo interpretado. ]
+
+Cada entrada nesse vetor de strings representa o conteúdo de uma linha do arquivo que o interpretador está interpretando.
+
+O interpretador também precisa saber qual é a linha atual que está sendo interpretada. 
+
+Os usuários do interpretador (outros desenvolvedores da sua equipe) precisam de algumas
+funcionalidades dele, mas seu gerente de projeto disse que existem restrições que eles precisam
+seguir. As funcionalidades que seus colegas querem é:
+    a) capacidade de obter uma variável da lista de variáveis do interpretador através do nome dessa variável;
+    b) saber quantas variáveis o interpretador possui em sua lista de variáveis;
+    c) a linha atual que o interpretador está interpretando no momento;
+    d) o número de linhas que o interpretador precisa interpretar para finalizar a interpretação do programa atual. 
+As restrições do seu gerente são: 
+    a) usuários do interpretador podem adicionar ou remover variáveis pelo nome delas, mas não podem acessar a
+lista de variáveis diretamente;
+    b) usuários do interpretador não podem alterar o conteúdo das linhas do programa sendo interpretado;
+    c) os usuários do interpretador não podem alterar a linha atual que o interpretador está interpretando.
 */
 
 public class Exe1 {
-    public static void main(String args[]){
-     
+    public static void main(String args[]) {
+
     }
-} 
+}
