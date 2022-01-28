abc56874210abec304bbdb47001826bad24b3700
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-01-28 14:44:12 -0300

    Identado e inserido métodos

--- L3/Exe1.java
+++ L3/Exe1.java
@@ -1,20 +1,118 @@
-abstract class Entidade { public int x,y; public abstract void posiciona(int x,int y);}
-interface Movel { public void moveX(int q); public void moveY(int q); }
-interface Atualizavel { public void atualiza(); }
-interface Serializavel { public int[] serializa(); public void deserializa(int[] v); }
-interface Combativel { public boolean podeCombater(Combativel c) }
+abstract class Entidade {
+    public int x, y;
+
+    public abstract void posiciona(int x, int y);
+}
+
+interface Movel {
+    public void moveX(int q);
+
+    public void moveY(int q);
+}
+
+interface Atualizavel {
+    public void atualiza();
+}
+
+interface Serializavel {
+    public int[] serializa();
+
+    public void deserializa(int[] v);
+}
+
+interface Combativel {
+    public boolean podeCombater(Combativel c)
+}
+
 class Unidade extends Entidade implements Movel, Atualizavel {
-public void atacar() { }
-public float distanciaBase() { }
-public boolean estahMorta() { }
+    public void atacar() {
+    }
+
+    public float distanciaBase() {
+    }
+
+    public boolean estahMorta() {
+    }
+
+    @Override
+    public void atualiza() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void moveX(int q) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void moveY(int q) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void posiciona(int x, int y) {
+        // TODO Auto-generated method stub
+
+    }
 }
+
 class Construcao extends Entidade implements Atualizavel {
-public void produzir() { }
+    public void produzir() {
+    }
+
+    @Override
+    public void atualiza() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void posiciona(int x, int y) {
+        // TODO Auto-generated method stub
+
+    }
 }
-class Infantaria extends Unidade implements Combativel { public void caminha(); }
-class Trabalhador extends Unidade { public void executaTarefa(); }
-class Arqueiro extends Infantaria { public void atiraFlecha(); }
-class Cavaleiro extends Infantaria { public void cavalga(); }
-class Bombeiro extends Infantaria { public void explode(); }
-class Ferraria extends Construcao { public void carrega(); }
-class Quartel extends Construcao { public void treina(); }
+
+class Infantaria extends Unidade implements Combativel {
+    public void caminha() {
+    };
+
+    @Override
+    public boolean podeCombater(Combativel c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
+
+class Trabalhador extends Unidade {
+    public void executaTarefa() {
+    };
+}
+
+class Arqueiro extends Infantaria {
+    public void atiraFlecha() {
+    };
+}
+
+class Cavaleiro extends Infantaria {
+    public void cavalga() {
+    };
+}
+
+class Bombeiro extends Infantaria {
+    public void explode() {
+    };
+}
+
+class Ferraria extends Construcao {
+    public void carrega() {
+    };
+}
+
+class Quartel extends Construcao {
+    public void treina() {
+    };
+}
--- L3/Exe5.java
+++ L3/Exe5.java
@@ -1,17 +1,120 @@
-interface Pausavel { public void pausa(); public void continua(); }
-interface Comparavel { public boolean igual(Comparavel c); }
-interface Clonavel { public Clonavel clona(); }
+interface Pausavel {
+    public void pausa();
+
+    public void continua();
+}
+
+interface Comparavel {
+    public boolean igual(Comparavel c);
+}
+
+interface Clonavel {
+    public Clonavel clona();
+}
+
 class App implements Pausavel, Comparavel, Clonavel {
-public void roda() { }
-public void destroi() { }
+    public void roda() {
+    }
+
+    public void destroi() {
+    }
+
+    @Override
+    public Clonavel clona() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean igual(Comparavel c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void pausa() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void continua() {
+        // TODO Auto-generated method stub
+
+    }
 }
+
 class Processo implements Pausavel {
-public void fork();
+    public void fork() {
+    };
+
+    @Override
+    public void pausa() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void continua() {
+        // TODO Auto-generated method stub
+
+    }
 }
-class Gui extends App { public void mostra(){} }
-class Hud extends Gui { public void atualiza(){} }
-class Mouse extends Gui { public void posiciona(){} }
-class Compacta extends App { public void recebe(){} }
-class Copia extends Compacta { public void copia(){} }
-class Thread extends Processo { public boolean acao(){} }
-class Disco implements Pausavel, Comparavel, Clonavel { }
+
+class Gui extends App {
+    public void mostra() {
+    }
+}
+
+class Hud extends Gui {
+    public void atualiza() {
+    }
+}
+
+class Mouse extends Gui {
+    public void posiciona() {
+    }
+}
+
+class Compacta extends App {
+    public void recebe() {
+    }
+}
+
+class Copia extends Compacta {
+    public void copia() {
+    }
+}
+
+class Thread extends Processo {
+    public boolean acao() {
+        return false;
+    }
+}
+
+class Disco implements Pausavel, Comparavel, Clonavel {
+
+    @Override
+    public Clonavel clona() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean igual(Comparavel c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void pausa() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void continua() {
+        // TODO Auto-generated method stub
+
+    }
+}
